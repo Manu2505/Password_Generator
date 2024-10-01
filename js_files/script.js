b4998fcdb490d65e8815690e4d4e0ba2d9250070
Manu2505 <148750849+Manu2505@users.noreply.github.com>
2024-10-01 15:24:54 +0200

    Password Generator Button works

--- js_files/script.js
+++ js_files/script.js
@@ -1,7 +1,7 @@
-const small_char = "abcdefghijklmnopqrstuvwxyz".split("");
-const large_char = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
-const numbers = "0123456789".split("");
-const special_chars = "!@#$%^&*()_+{}|:<>?".split("");
+const small_char = "abcdefghijklmnopqrstuvwxyz";
+const large_char = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numbers = "0123456789";
+const special_chars = "!@#$%^&*()_+{}|:<>?";
 
 window.addEventListener("load", function () {});
 
@@ -10,26 +10,29 @@ function show_value_of_range(range) {
 }
 
 function generate_password() {
-  var includeUppercase = document.getElementById("cap_letters").ckecked;
-  var includeLowercase = document.getElementById("small_letters").ckecked;
-  var includeNumbers = document.getElementById("numbers").ckecked;
-  var includeSpecialChars = document.getElementById("special_chars").ckecked;
-  var length = document.getElementById("show_range").valueAsNumber;
-
-  console.log("length: ", length);
+  var includeUppercase = document.getElementById("cap_letters").checked;
+  var includeLowercase = document.getElementById("small_letters").checked;
+  var includeNumbers = document.getElementById("numbers").checked;
+  var includeSpecialChars = document.getElementById("special_chars").checked;
+  
   console.log("includeUppercase: ", includeUppercase);
-
+  
   let allChars = "";
   if (includeUppercase) allChars += large_char;
   if (includeLowercase) allChars += small_char;
   if (includeNumbers) allChars += numbers;
   if (includeSpecialChars) allChars += special_chars;
-
+  
+  console.log("allChars: ", allChars);
+  
+  var length = document.getElementById("leng_pwd").valueAsNumber;
+  console.log("length: ", length);
+  
   let password = "";
   for (let i = 0; i < length; i++) {
     const randomIndex = Math.floor(Math.random() * allChars.length);
     password += allChars[randomIndex];
   }
-
+  console.log("password", password);
   document.getElementById("pwd_label").innerHTML = password;
 }
