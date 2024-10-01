d730e29a8a553c933327571d739a255dc0c2d96e
Manu2505 <148750849+Manu2505@users.noreply.github.com>
2024-10-01 15:36:11 +0200

    No checks Check is working now

--- js_files/script.js
+++ js_files/script.js
@@ -3,7 +3,7 @@ const large_char = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const numbers = "0123456789";
 const special_chars = "!@#$%^&*()_+{}|:<>?";
 
-window.addEventListener("load", function () {});
+//window.addEventListener("load", function () {});
 
 function show_value_of_range(range) {
   document.getElementById("show_range").innerHTML = range.valueAsNumber;
@@ -14,25 +14,30 @@ function generate_password() {
   var includeLowercase = document.getElementById("small_letters").checked;
   var includeNumbers = document.getElementById("numbers").checked;
   var includeSpecialChars = document.getElementById("special_chars").checked;
-  
-  console.log("includeUppercase: ", includeUppercase);
-  
+
+  if (
+    includeLowercase == false &&
+      includeUppercase == false &&
+      includeNumbers == false &&
+      includeSpecialChars == false
+  ) {
+    return (document.getElementById("pwd_label").innerHTML =
+      "Please select atleast one option");
+  }
+
   let allChars = "";
   if (includeUppercase) allChars += large_char;
   if (includeLowercase) allChars += small_char;
   if (includeNumbers) allChars += numbers;
   if (includeSpecialChars) allChars += special_chars;
-  
-  console.log("allChars: ", allChars);
-  
+
   var length = document.getElementById("leng_pwd").valueAsNumber;
-  console.log("length: ", length);
-  
+
   let password = "";
   for (let i = 0; i < length; i++) {
     const randomIndex = Math.floor(Math.random() * allChars.length);
     password += allChars[randomIndex];
   }
-  console.log("password", password);
+
   document.getElementById("pwd_label").innerHTML = password;
 }
