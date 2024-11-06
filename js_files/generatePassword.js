4e8a73f5f806007cdf3d94a9e74477d5fc0c11b4
ManuelHaeusser <haeusser-manuel@web.de>
2024-11-06 21:46:23 +0100

    Security password strength indication

--- js_files/generatePassword.js
+++ js_files/generatePassword.js
@@ -6,7 +6,8 @@ const special_chars = "!@#$%^&*()_+{}|:<>?";
 //window.addEventListener("load", function () {});
 
 function showValueOfRange(range) {
-  document.getElementById("showRange").innerHTML = range.valueAsNumber;
+console.log(range)
+  document.getElementById("showRange").innerHTML = range.value;
 }
 
 function generatePassword() {
@@ -31,7 +32,7 @@ function generatePassword() {
   if (includeNumbers) allChars += numbers;
   if (includeSpecialChars) allChars += special_chars;
 
-  var length = document.getElementById("lengPassword").valueAsNumber;
+  var length = document.getElementById("lengPassword").value;
 
   let password = "";
   for (let i = 0; i < length; i++) {
@@ -40,4 +41,42 @@ function generatePassword() {
   }
 
   document.getElementById("passwordLabel").innerHTML = password;
+
+  setSecurityInfo(length, includeUppercase, includeSpecialChars, includeNumbers, includeLowercase)
+}
+
+function setSecurityInfo(length, includeUppercase, includeSpecialChars, includeNumbers, includeLowercase){
+  var indicator = document.getElementById("security_bar");
+  var indicator_text = document.getElementById("security-text");
+
+
+  var color = "";
+  var size = "";
+  var text ="";
+  if(length <= 6 || (length ===7 && !includeNumbers && !includeSpecialChars) || (length ===8 && !includeUppercase && !includeNumbers && !includeSpecialChars)){
+    color="red";
+    size="10%";
+    text="Password is bad, you need to add more characters!";
+  }else if(length <=8 || (length <=10 && !includeNumbers && !includeSpecialChars && !includeUppercase)){
+    color="orange";
+    size="30%";
+    text="Password is not yet good, you should add more characters!";
+  }else if(length <=9 || (length <=12 && !includeNumbers && !includeSpecialChars && !includeUppercase)){
+    color="yellow";
+    size="50%";
+    text="Password is OK, but could be improved!";
+  }else if(length > 12 && includeLowercase && includeNumbers && includeSpecialChars && includeUppercase){
+     color="lightgreen";
+     size="100%";
+     text="Password is Perfect!";
+  }else{
+     color="green";
+     size="75%";
+     text="That is a good Password!";
+  }
+
+  indicator.style.backgroundColor = color;
+  indicator.style.width = size;
+
+  indicator_text.innerHTML = text;
 }
